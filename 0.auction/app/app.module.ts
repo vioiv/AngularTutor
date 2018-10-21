954efe105c2c335339b0b21a88206027853fde9a
JeongHoon, Kim <devkjh@gmail.com>
2018-10-21 22:04:07 +0900

    add application and product-item

---
+++ 0.auction/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import ApplicationComponent from './components/application/application.component';
+import CarouselComponent from './components/carousel/carousel.component';
+import FooterComponent from './components/footer/footer.component';
+import NavbarComponent from './components/navbar/navbar.component';
+import ProductItemComponent from './components/product-item/product-item.component';
+import SearchComponent from './components/search/search.component';
+import StarsComponent from './components/stars/stars.component';
+
+@NgModule({
+	imports : [BrowserModule],
+	declarations : [ApplicationComponent,
+		CarouselComponent,
+		FooterComponent,
+		NavbarComponent,
+		ProductItemComponent,
+		SearchComponent,
+		StarsComponent],
+	providers : [ProductService],
+	bootstrap : [AppComponent]
+})
+export class AppModule {}
